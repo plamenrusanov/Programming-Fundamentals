cab692af3f02cb214c7e81dd5531ddd59722d993
plamenrusanov <plamen.rusanov@abv.bg>
2017-05-25 07:10:12 +0300

    new

---
+++ Intro and Basic Syntax - Exercises/01. Debit Card Number/DebitCardNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Debit_Card_Number
+{
+    class DebitCardNumber
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstNumber:d4} {secondNumber:d4} {thirdNumber:d4} {fourNumber:d4}");
+
+        }
+    }
+}
