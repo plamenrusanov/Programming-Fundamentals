e3372add88600177097f0e6a9ca002fd6cb4ef0a
plamenrusanov <plamen.rusanov@abv.bg>
2017-05-26 21:54:45 +0300

    new exercises

---
+++ Lab Data Types Numeral Types and Type Conversion/04. Elevator/Elevator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Elevator
+{
+    class Elevator
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}", (int)Math.Ceiling((double)persons / capacity));
+        }
+    }
+}
