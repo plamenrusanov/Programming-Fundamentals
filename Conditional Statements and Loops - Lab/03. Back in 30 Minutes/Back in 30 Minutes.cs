ce4df1db1c1902785d1c0e327f4b9ff0cde2690c
plamenrusanov <plamen.rusanov@abv.bg>
2017-05-30 07:09:24 +0300

    new lab

---
+++ Conditional Statements and Loops - Lab/03. Back in 30 Minutes/Back in 30 Minutes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Back_in_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+            hours += minutes / 60;
+            minutes = minutes % 60;
+            hours = hours >= 24 ? 0 : hours;
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
---
+++ Conditional Statements and Loops - Lab/04. Month Printer/Month Printer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Month_Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mount = int.Parse(Console.ReadLine());
+
+            string abc = null;
+            switch (mount)
+            {
+                case 1: abc = "January"; break;
+                case 2: abc = "February"; break;
+                case 3: abc = "March"; break;
+                case 4: abc = "April"; break;
+                case 5: abc = "May"; break;
+                case 6: abc = "June"; break;
+                case 7: abc = "July"; break;
+                case 8: abc = "August"; break;
+                case 9: abc = "September"; break;
+                case 10: abc = "October"; break;
+                case 11: abc = "November"; break;
+                case 12: abc = "Desember"; break;
+                default:
+                    abc = "Error!";
+                    break;
+            }
+            Console.WriteLine(abc);
+        }
+    }
+}
