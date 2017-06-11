bc69bafc9510d5c5e626e6e4c6301d0064b1bacb
plamenrusanov <plamen.rusanov@abv.bg>
2017-06-11 19:14:51 +0300

    add exercises

---
+++ Data Types - Exercises/16. Tricky Strings/TrickyStrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Tricky_Strings
+{
+    class TrickyStrings
+    {
+        static void Main(string[] args)
+        {
+            string delimeter = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            string text = string.Empty;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string word = Console.ReadLine();
+                if (i != n)
+                {
+                    text += word + delimeter;
+                }
+                else
+                {
+                    text += word;
+                }
+            }
+            Console.WriteLine(text.Trim());
+        }
+    }
+}
---
+++ Data Types - Exercises/17.  Cypher Roulette/CypherRoulette.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Cypher_Roulette
+{
+    class CypherRoulette
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int spinCount = 0;
+            string totalString = string.Empty;
+            string lastString = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string newInputString = Console.ReadLine();
+
+
+                if (newInputString == "spin")
+                {
+                    spinCount++;
+                    i--;
+                }
+                if (spinCount % 2 == 0 && newInputString != "spin")
+                {
+                    totalString = totalString + newInputString;
+                }
+                else if (spinCount % 2 == 1 && newInputString != "spin")
+                {
+                    totalString = newInputString + totalString;
+                }
+                if (newInputString == lastString)
+                {
+                    totalString = string.Empty;
+                    if (newInputString == "spin")
+                    {
+                        spinCount++;
+                    }
+                }
+                lastString = newInputString;
+
+            }
+            Console.WriteLine(totalString);
+        }
+    }
+}
