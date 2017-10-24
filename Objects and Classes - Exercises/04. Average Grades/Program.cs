1803a42276855739b4964a8610264714d6877d12
plamenrusanov <plamen.rusanov@abv.bg>
2017-10-24 22:49:49 +0300

    clases exercises

---
+++ Objects and Classes - Exercises/04. Average Grades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Average_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            List<Student> students = new List<Student>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                students.Add(new Student(input));               
+            }
+            students = students.Where(s => s.Average >= 5).ToList();
+            foreach (var item in students.OrderBy(s => s.Name).ThenByDescending(s => s.Average))
+            {
+                string name = item.Name;
+                double average = item.Average;
+                Console.WriteLine($"{name} -> {average:f2}");                           
+            }           
+        }
+        public class Student
+        {
+            public string Name { get; set; }
+            public List<double> Grade { get; set; }
+            public double Average { get; set; }
+            public Student(string[] input)
+            {
+                Name = input[0];
+                Grade = input.Skip(1).Take(input.Length - 1).Select(double.Parse).ToList();            
+                Average = Grade.Average();
+            }
+        }
+    }
+   
+}
---
+++ Objects and Classes - Exercises/05. Book Library/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Book_Library
+{
+    using System.Globalization;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+            }
+        }
+    }
+    class Library
+    {
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+    }
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime Date { get; set; }
+        public string ISBN { get; set; }
+        public double Price { get; set; }
+        public Book(string[] input)
+        {
+            Title = input[0];
+            Author = input[1];
+            Publisher = input[2];
+            Date = DateTime.ParseExact(input[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            ISBN = input[4];
+            Price = double.Parse(input[5]);
+        }
+    }
+}
